eafa6a2d6f7f754c6332516fa870f1341a2581c7
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-02-28 17:56:53 -0600

    Completed

--- js/itunesService.js
+++ js/itunesService.js
@@ -2,13 +2,55 @@ var app = angular.module('itunes');
 
 app.service('itunesService', function($http, $q){
   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+  //Also note that we're using a 'service' and not a 'factory' so all your methods 
+  //you want to call in your controller need to be on 'this'.
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+  //Write a method that accepts an artist's name as the parameter, then makes a '
+  //JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
   //Note that in the above line, artist is the parameter being passed in. 
-  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+  //You can return the http request or you can make your own promise in order to 
+  //manipulate the data before you resolve it.
 
-    //Code here
-    
+  this.artist = function(name) {
+    var deferred = $q.defer();
+    $http({
+      method: "JSONP",
+      url: 'https://itunes.apple.com/search?term=;' + name + '&callback=JSON_CALLBACK'
+    }).then(function(response) {
+      var res = response.data.results;
+      
+      
+      var arr = [];
+      for (var i = 0; i < res.length; i++) { //had to loop trough response.data.results
+        arr.push({
+        Artist: res[i].artistName, 
+        Collection: res[i].collectionName, 
+        AlbumArt: res[i].artworkUrl100, 
+        Type: res[i].wrapperType,  
+        CollectionPrice: res[i].collectionPrice
+      });
+      }
+
+      deferred.resolve(arr);
+    })
+    return deferred.promise;
+  }
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,31 +2,48 @@ var app = angular.module('itunes');
 
 app.controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is
-  //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
-  //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
+  //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData 
+  //on $scope and is putting whatever songData is into the grid.
+  //this means when you make your iTunes request, you'll need to get back the information, 
+  //parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
   $scope.gridOptions = { 
       data: 'songData',
-      height: '110px',
+      rowHeight: '100', //ui-grid does not take px or percentages or auto just a plain number
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
       columnDefs: [
-        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+        // {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
         {field: 'Artist', displayName: 'Artist'},
         {field: 'Collection', displayName: 'Collection'},
-        {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
+        {field: 'AlbumArt', displayName: 'Album Art', width:'100', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img ng-src="{{grid.getCellValue(row, col)}}"></div>'},
         {field: 'Type', displayName: 'Type'},
         {field: 'CollectionPrice', displayName: 'Collection Price'},
       ]
   };
 
-  //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
+  //Our controller is what's going to connect our 'heavy lifting' itunesService with our 
+  //view (index.html) so our user can see the results they get back from itunes.
 
   //First inject itunesService into your controller.
 
-    //code here
+  $scope.getSongData = function() {
+    
+    itunesService.artist($scope.artist).then(function(songArray){
+      $scope.songData = songArray; 
+    })
+
+  }
+
+ 
 
 
-  //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
-  //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
+
+
+
+  //Now write a function that will call the method on the itunesService that is responsible 
+  //for getting the data from iTunes, whenever the user clicks the submit button
+  //*remember, that method should be expecting an artist name. The artist name is coming 
+  //from the input box on index.html, head over there and check if that input box is tied to 
+  //any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
     //Code here
